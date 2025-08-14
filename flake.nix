e7c60b25b50bba61c1e6c4a8c9bded6400b907f6
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-14 05:28:44 -0400

    style: format flake.nix

--- flake.nix
+++ flake.nix
@@ -3,12 +3,25 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
         devShells = {
-          default = pkgs.mkShell { packages = with pkgs; [ python313 ]; };
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              python313
+            ];
+          };
         };
-      });
+      }
+    );
 }
