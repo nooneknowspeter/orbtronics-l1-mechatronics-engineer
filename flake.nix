10fabdaef3f7f9cca639b8ebdd89d9c633c8a78d
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-15 19:11:08 -0400

    chore(nix): add dockfmt to flake.nix

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
             packages = with pkgs; [
               # formatters
               black
+              dockfmt
               isort
               nixfmt-rfc-style
               prettier
