e962ec261b611f00ba3bf589508be6c2bb85ee95
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-15 18:57:39 -0400

    chore(simulation): initialize device name using parameter in class constructor

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -8,16 +8,16 @@ import dotenv
 import requests
 
 
-    device_name: str = "device001"
 class Device:
+    device_name: str = ""
     temperature: float = 0
     humidity: float = 0
 
-    def __init__(self) -> None:
-        None
-
     def randomValue(self, start: int, end: int, roundNDigits: int = 2) -> float:
         return round(random.uniform(start, end), roundNDigits)
+
+    def __init__(self, device_name: str) -> None:
+        self.device_name = device_name
 
     def updateValues(self) -> None:
         self.humidity = self.randomValue(18, 35, 2)
